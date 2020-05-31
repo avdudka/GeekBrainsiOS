cc47f88df36184638284c9bae7de46bf7221a282
Andrey Dudka <andrew@dudka.su>
2020-05-31 23:05:06 +0300

    Name change

---
+++ 1l_DudkaAndrey_extension/Lesson1Task3/DollarTextFieldDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  DollarTextFieldDelegate.swift
+//  Lesson1Task3
+//
+//  Created by Andrey on 31.05.2020.
+//  Copyright © 2020 Andrey Dudka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DollarTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text! as NSString
+        var newText = oldText.replacingCharacters(in: range, with: string)
+        let newTextString = String(newText)
+        let digits = CharacterSet.decimalDigits
+        var digitText = ""
+        for c in (newTextString.unicodeScalars) {
+            if digits.contains(UnicodeScalar(c.value)!) {
+                digitText.append("\(c)")
+            }
+        }
+        if let num = Int(digitText) {
+            newText = String(num)
+        } else {
+            newText = ""
+        }
+        textField.text = newText
+        return false
+    }
+}
---
+++ 1l_DudkaAndrey_extension/Lesson1Task3/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Lesson1Task3
+//
+//  Created by Andrey on 31.05.2020.
+//  Copyright © 2020 Andrey Dudka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var depositAmount: UITextField!
+    @IBOutlet weak var depositPercent: UITextField!
+    
+    @IBOutlet weak var resultAmountWithCap: UILabel!
+    @IBOutlet weak var resultAmountNoCap: UILabel!
+    
+    var dollarDelegate = DollarTextFieldDelegate()
+    var percentDelegate = PercentTextFieldDelegate()
+    
+    var duration = 5
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        depositAmount.text = ""
+        depositAmount.delegate = self.dollarDelegate
+        depositPercent.text = ""
+        depositPercent.delegate = self.percentDelegate
+        resultAmountNoCap.text = "0.0"
+        resultAmountWithCap.text = "0.0"
+    }
+    
+    @IBAction func countPercent() {
+        if depositAmount.text != "" && depositPercent.text != "" {
+            let amount = Double(depositAmount.text!)
+            let percent = Double(depositPercent.text!)
+            resultAmountWithCap.text = String(countDepositWCap(amount: amount!, percent: percent!))
+            resultAmountNoCap.text = String(countDepositWOCap(amount: amount!, percent: percent!))
+        }
+        self.view.endEditing(true)
+    }
+    
+    func countDepositWCap(amount: Double, percent: Double) -> Double {
+        var newAmount = amount
+        for _ in 1...5 {
+            newAmount = newAmount + newAmount*percent/100}
+        return round(newAmount*100)/100
+    }
+    
+    func countDepositWOCap(amount: Double, percent: Double) -> Double {
+        let newAmount = amount + 5 * amount*percent/100
+        return round(newAmount*100)/100
+    }
+    
+}
